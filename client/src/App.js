798662cc61f84bb7c36568cde2a4088324042659
Buligan-Răzvan-Mihai <31895194+BuliganRazvanMihai@users.noreply.github.com>
2021-04-13 00:09:41 +0300

    Protecting routes
    When a user is not logged in, he will be redirected to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
 import './App.css';
 
-const App = () => (
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
   <Provider store={store}>
       <Router>
       <Fragment>
@@ -23,11 +36,12 @@ const App = () => (
           <Switch className="container">
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
           </Switch>
         </section>
       </Fragment>
       </Router>
       </Provider>
-);
+)};
 
 export default App;
